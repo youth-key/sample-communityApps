3c2643c802874b8e7421632a22f0b52cfa74855c
Izumi Yusuke <cosmosas11@gmail.com>
2019-06-18 16:01:24 +0900

    sample-communityApps by Yusuke.i

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+
+<head>
+    <title>コミュニティサイト｜トップページ</title>
+    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>コミュニティサイト</h1>
+</body>
+
+</html>
